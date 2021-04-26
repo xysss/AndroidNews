6040a3945a7552ba7b140739a7da66a3aba6c579
xys <1343025166@qq.com>
2021-04-26 17:59:25 +0800

    add BaseApp

---
+++ JetpackMvvm/src/main/java/com/xysss/jetpackmvvm/base/Ktx.kt
@@ -0,0 +1,6 @@
+package com.xysss.jetpackmvvm.base
+
+/**
+ * Author:bysd-2
+ * Time:2021/4/2517:59
+ */
---
+++ JetpackMvvm/src/main/java/com/xysss/jetpackmvvm/ext/BaseViewModelExt.kt
@@ -0,0 +1,6 @@
+package com.xysss.jetpackmvvm.ext
+
+/**
+ * Author:bysd-2
+ * Time:2021/4/2518:01
+ */
