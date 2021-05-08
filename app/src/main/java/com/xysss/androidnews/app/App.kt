f9dc8dd87f201e8ce895d32fc24657606edeecfe
xys <1343025166@qq.com>
2021-05-08 18:01:11 +0800

    add Bugly更新信息和ndk配置

--- app/src/main/java/com/xysss/androidnews/app/App.kt
+++ app/src/main/java/com/xysss/androidnews/app/App.kt
@@ -54,7 +54,6 @@ class App : BaseApp() {
             .addCallback(EmptyCallback())//空
             .setDefaultCallback(SuccessCallback::class.java)//设置默认加载状态页
             .commit()
-        //初始化Bugly
         val context = applicationContext
         // 获取当前包名
         val packageName = context.packageName
@@ -64,7 +63,7 @@ class App : BaseApp() {
         val strategy = CrashReport.UserStrategy(context)
         strategy.isUploadProcess = processName == null || processName == packageName
         // 初始化Bugly
-        Bugly.init(context, if (BuildConfig.DEBUG) "xxx" else "a52f2b5ebb", BuildConfig.DEBUG)
+        Bugly.init(context, if (BuildConfig.DEBUG) "de489df03b" else "", BuildConfig.DEBUG)
         "".logd()
         jetpackMvvmLog = BuildConfig.DEBUG
 
@@ -73,11 +72,11 @@ class App : BaseApp() {
 
         //防止项目崩溃，崩溃后打开错误界面
         CaocConfig.Builder.create()
-            .backgroundMode(CaocConfig.BACKGROUND_MODE_SILENT) //default: CaocConfig.BACKGROUND_MODE_SHOW_CUSTOM
+            .backgroundMode(CaocConfig.BACKGROUND_MODE_SILENT) //default: CaocConfig.BACKGROUND_MODE_SHOW_CUSTOM 背景模式,开启沉浸式
             .enabled(true)//是否启用CustomActivityOnCrash崩溃拦截机制 必须启用！不然集成这个库干啥？？？
-            .showErrorDetails(false) //是否必须显示包含错误详细信息的按钮 default: true
-            .showRestartButton(false) //是否必须显示“重新启动应用程序”按钮或“关闭应用程序”按钮default: true
-            .logErrorOnRestart(false) //是否必须重新堆栈堆栈跟踪 default: true
+            .showErrorDetails(true) //是否必须显示包含错误详细信息的按钮 default: true
+            .showRestartButton(true) //是否必须显示“重新启动应用程序”按钮或“关闭应用程序”按钮default: true
+            .logErrorOnRestart(true) //是否必须重新堆栈堆栈跟踪 default: true
             .trackActivities(true) //是否必须跟踪用户访问的活动及其生命周期调用 default: false
             .minTimeBetweenCrashesMs(2000) //应用程序崩溃之间必须经过的时间 default: 3000
             .restartActivity(WelcomeActivity::class.java) // 重启的activity
